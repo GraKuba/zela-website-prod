55fb4aef99e5d20fbbdb812c575cd567bd92ac48
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2025-07-03 23:53:57 +0200

    Made major changes to the website. Landing page is now complete. About page has been created. Blog page has been created. Contact page has been created.

--- Zela/website/views.py
+++ Zela/website/views.py
@@ -6,3 +6,18 @@ def home(request):
     return render(request, 'website/home.html', {
         'title': 'Zela - Home',
     })
+
+def about(request):
+    return render(request, 'website/components/page-about/about.html', {
+        'title': 'About Zela - Attention and care for your home',
+    })
+
+def blog(request):
+    return render(request, 'website/components/page-blog/blog.html', {
+        'title': 'Zela Blog - Tips, news and household inspiration',
+    })
+
+def contact(request):
+    return render(request, 'website/components/page-contact/contact.html', {
+        'title': 'Contact Zela - Get in Touch',
+    })
